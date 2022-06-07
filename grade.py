40bbfb6a19df8810e60c1beeb724855dcf9bd55d
nnaatti <101382049+nnaatti@users.noreply.github.com>
2022-06-07 04:53:11 -0700

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,31 @@
+number_of_students = int(input(f'how many students   '))
+
+#list_of_students = [number_of_students]
+
+#number_of_students[list_of_students], number_of_students[list_of_students]
+
+number_of_students == number_of_students 
+
+list_of_students = input('please wnter your name   ')
+
+age = int(input(f'hello {list_of_students} please enter your age       '))
+
+mark = int(input('please enter you mark    '))
+
+student_mark = [list_of_students,mark]
+
+
+
+
+def grade(mark:int):
+    if mark < 60 :
+        print(f'sorry {list_of_students} you have faild')
+
+    elif mark < 80:
+        print(f'{list_of_students} you passed')
+
+    elif mark > 80:
+        print(f'{list_of_students} good job!')    
+
+grade(mark)
+
